253c04adf21f13d511f0a7a7a6f9ef319ff50ed6
Shared1Vegetarian <77779428+Shared1Vegetarian@users.noreply.github.com>
2021-01-21 22:38:39 +0200

    2.12 Working with Enums

--- app.js
+++ app.js
@@ -2,11 +2,31 @@
 //    name: string;
 //    age: number;
 // } = {
+// const person: {
+//    name: string;
+//    age: number;
+//    hobbies: string[];
+//    role: [number, string];
+// } = {
+//    name: "Maximilian",
+//    age: 30,
+//    hobbies: ["Sports", "Cooking"],
+//    role: [2, "author"],
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 4] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 5] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Maximilian",
     age: 30,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"]
+    role: Role.ADMIN
 };
 // person.role.push("admin");
 // person.role[1] = 10;
@@ -19,4 +39,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     //console.log(hobby.map()); // ERROR
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
