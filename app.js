b1e0e8f9ecbe5b8eeacdaa20368c7386728c45fc
Shared1Vegetarian <77779428+Shared1Vegetarian@users.noreply.github.com>
2021-01-21 21:51:38 +0200

    2.10 Array Types

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-function add(n1, n2, showResult, phrase) {
-    if (showResult) {
-        var result = n1 + n2;
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
+// const person: {
+//    name: string;
+//    age: number;
+// } = {
+var person = {
+    name: "Maximilian",
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
+};
+var favoriteActivities;
+favoriteActivities = ["sports"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); // ERROR
 }
-var number1;
-number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,16 +1,19 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-   if (showResult) {
-      const result = n1 + n2;
-      console.log(phrase + result);
-   } else {
-      return n1 + n2;
-   }
+// const person: {
+//    name: string;
+//    age: number;
+// } = {
+const person = {
+   name: "Maximilian",
+   age: 30,
+   hobbies: ["Sports", "Cooking"],
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ["sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+   console.log(hobby.toUpperCase());
+   //console.log(hobby.map()); // ERROR
 }
-
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-add(number1, number2, printResult, resultPhrase);
