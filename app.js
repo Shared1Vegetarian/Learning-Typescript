efc6ee5c71db303f8a8253d73976d958069f7976
Shared1Vegetarian <77779428+Shared1Vegetarian@users.noreply.github.com>
2021-01-22 16:21:11 +0200

    2.24 The never Type

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ userInput = "Max";
 if (typeof userInput === "string") {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true);
+}
+var result = generateError("An error occurred!", 500);
+console.log(result);
